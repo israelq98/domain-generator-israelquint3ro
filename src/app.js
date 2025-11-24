bd5b6a03a7795c1876975e7e3cff74641c81c2c1
Israel Quintero <ijsalasar@hotmail.com>
2025-11-24 13:40:08 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our', 'los', 'grupo', 'asossiation'];
+let adj = ['main', 'ultimate', 'wonderful', 'rapids', 'intelligent'];
+let noun = ['jogger', 'mexicanos', 'footballer', 'campesinos', 'geeks'];
+let extension = ['.com','.es','.ios','es','.de','.pa']
+
+function domainGenerator() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++){
+        for(let m = 0; m <extension.length; m++){
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${extension[m]}`);
+
+        }
+      }
+    }
+
+  }
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+domainGenerator()
