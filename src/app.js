660587cbc322b29a0a7c48f4333d8a339063eb00
Israel Quintero <ijsalasar@hotmail.com>
2025-11-28 09:12:14 +0000

    updates

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 let pronoun = ['the', 'our', 'los', 'grupo', 'asossiation'];
 let adj = ['main', 'ultimate', 'wonderful', 'rapids', 'intelligent'];
 let noun = ['jogger', 'mexicanos', 'footballer', 'campesinos', 'geeks'];
-let extension = ['.com','.es','.ios','.es','.de','.pa']
+let extension = ['.com', '.es', '.ios', '.es', '.de', '.pa']
 
 function domainGenerator() {
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++){
-        for(let m = 0; m <extension.length; m++){
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < extension.length; m++) {
           console.log(`${pronoun[i]}${adj[j]}${noun[k]}${extension[m]}`);
 
         }
